42a6f9786f8c12134fa8e16f53a9a142b8b627cf
JOSEruben24 <80548962+JOSEruben24@users.noreply.github.com>
2021-03-12 15:20:16 -0600

    Add files via upload

---
+++ wsparte1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="wsparte1.aspx.cs" Inherits="wsparte1.wsparte1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Num1<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />Num2<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+
+            <br /><asp:Button ID="Button1" runat="server" Text="Suma" OnClick="Button1_Click" />
+            <br />
+            <br /><asp:Button ID="Button3" runat="server" Text="Multiplicacion" OnClick="Button1_Click" />
+            <br /><asp:Button ID="Button4" runat="server" Text="Division" OnClick="Button1_Click" />
+            <br /><asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+    <p>
+        &nbsp;</p>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ wsparte1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace wsparte1
+{
+    public partial class wspart1 : System.Web.UI.Page
+    {
+        wsparte1.CalculatorSoapClient ws = new wsparte1.CalculatorSoapClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {    
+           Label1.Text = ws.Add(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Text = ws.Subtract(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Label1.Text = ws.Multiply(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Label1.Text = ws.Divide(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+    }
+}
